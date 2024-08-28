9a116e1281839b5d38cb560241b3f2eb68b232b7
Onur Gündoğdu <gundogduonur@outlook.com.tr>
2024-08-28 09:39:29 +0300

    added expired of connection

--- DatabaseConnection.java
+++ DatabaseConnection.java
@@ -1,8 +1,17 @@
 public class DatabaseConnection {
+
+    private long creationTime;
+
+    public DatabaseConnection() {
+        this.creationTime = System.currentTimeMillis();
+    }
     public void connect(){
         System.out.println("Connected to Database");
     }
     public void disconnect(){
         System.out.println("Disconnected from Database");
+    }
+    public boolean isExpired(long timeOut){
+        return (System.currentTimeMillis() - this.creationTime) > timeOut;
     }
 }
