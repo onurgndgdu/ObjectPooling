e369b4b4f910f0013f60fc3d4d584520b4d9467e
Onur Gündoğdu <gundogduonur@outlook.com.tr>
2024-08-28 09:27:11 +0300

    created databaseconnection class

---
+++ DatabaseConnection.java
@@ -0,0 +1,8 @@
+public class DatabaseConnection {
+    public void connect(){
+        System.out.println("Connected to Database");
+    }
+    public void disconnect(){
+        System.out.println("Disconnected from Database");
+    }
+}
